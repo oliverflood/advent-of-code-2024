f3b89c06c08f2081505ddd3889e32f841ed0a9fa
oliverflood <oliveroldrich@gmail.com>
2024-12-07 21:52:12 -0800

    days 7, 8

---
+++ day07b.py
@@ -0,0 +1,16 @@
+f = open("input.txt", "r")
+
+lines = f.read()
+lines = [line.split(': ') for line in lines.split('\n')]
+
+left = [int(line[0]) for line in lines]
+right = [list(map(int, line[1].split(' '))) for line in lines]
+
+def recurse(l, r, curr):
+	if len(r) == 0:
+		return l == curr
+
+	return recurse(l, r[1:], curr + r[0]) or recurse(l, r[1:], curr * r[0]) or recurse(l, r[1:], int(str(curr)+str(r[0])))
+
+ans = sum(recurse(left[i], right[i][1:], right[i][0]) * left[i] for i in range(len(left)))
+print(ans)
---
+++ day08a.py
@@ -0,0 +1,41 @@
+f = open("input.txt", "r")
+
+lines = f.read()
+tester = lines
+lines = [[c for c in line] for line in lines.split('\n')]
+
+pos_dict = {}
+for r in range(len(lines)):
+	for c in range(len(lines[0])):
+		if lines[r][c] != '.':
+			if lines[r][c] not in pos_dict:
+				pos_dict[lines[r][c]] = []
+			pos_dict[lines[r][c]].append([r, c])
+		
+def in_bounds(x):
+	return (0 <= x < len(lines))
+
+tester = [['.' for c in line] for line in tester.split()]
+vals = set()
+
+for key, value in pos_dict.items():
+	for i in range(len(value)):
+		for j in range(len(value)):
+			if i <= j:
+				continue
+			diff_r = value[i][0] - value[j][0]
+			diff_c = value[i][1] - value[j][1]
+
+			tests = []
+			tests.append([value[i][0] + diff_r, value[i][1] + diff_c])
+			tests.append([value[j][0] - diff_r, value[j][1] - diff_c])
+			
+			for test in tests:
+				if in_bounds(test[0]) and in_bounds(test[1]):
+					tester[test[0]][test[1]] = '#'
+					vals.add((test[0], test[1]))
+
+# for line in tester:
+# 	print(''.join(line))
+
+print(len(vals))
